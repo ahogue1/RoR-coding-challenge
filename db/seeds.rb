0ab668440ca3923e2f6fd60385d8e8065cd9fd83
Amy Hogue <hogue.a@gmail.com>
2019-06-08 14:51:30 -0700

    Create seeds for House, BannerPerson, Handout and LoyaltyPoint
    Using Faker's Game of Thrones generator.
    Create seeds to generate random values trying to stick to Readme specifications
    of probabilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Emptying Database..."
+
+House.destroy_all
+
+puts "Creating Seeds..."
+
+4.times do
+  house = House.create(name: Faker::TvShows::GameOfThrones.unique.house)
+
+  puts "Creating '#{house.name}' House..."
+
+  2.times do
+    banner_person = BannerPerson.create(
+      name: Faker::TvShows::GameOfThrones.unique.character,
+      house: house
+    )
+
+    # Generate 8 to 12 (2 to 3 times per week) unique random dates
+    random_days = (1..30).to_a.shuffle.take(rand(8..12))
+    random_days.each do |day|
+      # Skip 10% of handouts
+      if rand(1..10) > 1
+        Handout.create(
+          banner_person: banner_person,
+          value: rand(2..200) * 100,
+          date: Date.new(2019, 6, day)
+        )
+      end
+    end
+
+    # Generate 4 random almost weekly dates
+    random_days = [rand(1..7), rand(8..14), rand(15..22), rand(23..30)]
+    random_days.each do |day|
+      LoyaltyPoint.create(
+        banner_person: banner_person,
+        value: rand(5.0..18.0).round(1),
+        date: Date.new(2019, 6, day)
+      )
+    end
+  end
+end
+
+puts "Seeds Created"
