60649e0999883fded04a2f4a7a77c2e1ee4e8346
Amy Hogue <hogue.a@gmail.com>
2019-06-08 22:07:22 -0700

    Show BannerPerson loyalty_points and handouts in BannerPerson#show table

--- app/controllers/banner_people_controller.rb
+++ app/controllers/banner_people_controller.rb
@@ -2,4 +2,11 @@ class BannerPeopleController < ApplicationController
   def index
     @banner_people = BannerPerson.all
   end
+
+  def show
+    @banner_person = BannerPerson.find(params[:id])
+    @handouts = @banner_person.handouts
+    @loyalty_points = @banner_person.loyalty_points
+    @date = Date.today
+  end
 end
