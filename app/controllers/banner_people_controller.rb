b1e6b21978311e34af131058f0711739925786c4
Amy Hogue <hogue.a@gmail.com>
2019-06-09 16:50:46 -0700

    Extract BannerPerson#_table logic to controller method

--- app/controllers/banner_people_controller.rb
+++ app/controllers/banner_people_controller.rb
@@ -7,6 +7,32 @@ class BannerPeopleController < ApplicationController
     @banner_person = BannerPerson.find(params[:id])
     @handouts = @banner_person.handouts
     @loyalty_points = @banner_person.loyalty_points
-    @date = Date.today
+    @data = banner_person_monthly_data(Date.today, @banner_person)
+  end
+
+  private
+
+  def banner_person_monthly_data(date, banner_person)
+    data = []
+    days_in_month = date.end_of_month.day
+
+    (1..days_in_month).each do |day|
+      date = Date.new(date.year, date.month, day)
+
+      handout = banner_person.handouts.find { |handout| handout.date == date }
+      loyalty_point = banner_person.loyalty_points.find do |loyalty_point|
+        loyalty_point.date == date
+      end
+
+      if handout || loyalty_point
+        data.push({
+          date: date,
+          handout: handout,
+          loyalty_point: loyalty_point
+        })
+      end
+    end
+
+    data
   end
 end
