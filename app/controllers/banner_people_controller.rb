62ee8b55fccbf2d990615ac83d759fb24c58bd4a
Amy Hogue <hogue.a@gmail.com>
2019-06-09 21:10:27 -0700

    Add Advisements to BannerPerson#_table

--- app/controllers/banner_people_controller.rb
+++ app/controllers/banner_people_controller.rb
@@ -7,25 +7,27 @@ class BannerPeopleController < ApplicationController
     @banner_person = BannerPerson.find(params[:id])
     @handouts = @banner_person.handouts
     @loyalty_points = @banner_person.loyalty_points
-    @data = banner_person_monthly_data(Date.today, @banner_person)
+    @graph_data = graph_data(Date.today, @banner_person)
+    @table_data = table_data(@banner_person.advisements, @graph_data)
 
-    gon.dates = @data.map { |datum| datum[:date].strftime("%b %d") }
-    gon.handouts = @data.map { |datum|
+    gon.dates = @graph_data.map { |datum| datum[:date].strftime("%b %d") }
+    gon.handouts = @graph_data.map { |datum|
       datum[:handout] ? datum[:handout].value : nil
     }
-    gon.loyalty_points = @data.map.with_index { |datum, index|
+    gon.loyalty_points = @graph_data.map.with_index { |datum, index|
       datum[:loyalty_point] ? [index, datum[:loyalty_point].value] : nil
     }.compact
   end
 
   private
 
-  def banner_person_monthly_data(date, banner_person)
-    data = []
+  def graph_data(date, banner_person)
+    graph_data = []
     days_in_month = date.end_of_month.day
 
     (1..days_in_month).each do |day|
       date = Date.new(date.year, date.month, day)
+
 
       handout = banner_person.handouts.find { |handout| handout.date == date }
       loyalty_point = banner_person.loyalty_points.find do |loyalty_point|
@@ -33,7 +35,7 @@ class BannerPeopleController < ApplicationController
       end
 
       if handout || loyalty_point
-        data.push({
+        graph_data.push({
           date: date,
           handout: handout,
           loyalty_point: loyalty_point
@@ -41,6 +43,24 @@ class BannerPeopleController < ApplicationController
       end
     end
 
-    data
+    graph_data
+  end
+
+  def table_data(advisements, graph_data)
+    table_data = graph_data.deep_dup
+
+    advisements.each do |advisement|
+      datum = table_data.find { |datum| datum[:date] == advisement.date }
+      if datum
+        datum[:advisement] = advisement
+      else
+        table_data.push({
+          date: advisement.date,
+          advisement: advisement
+        })
+      end
+    end
+
+    table_data.sort { |a, b| a[:date] <=> b[:date] }
   end
 end
