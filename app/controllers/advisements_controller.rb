fdc7456e7abdadbb9b45692c83766c5dcf55fe22
Amy Hogue <hogue.a@gmail.com>
2019-06-11 18:44:31 -0700

    Add Modal for Edit Advisement

---
+++ app/controllers/advisements_controller.rb
@@ -0,0 +1,22 @@
+class AdvisementsController < ApplicationController
+
+  def update
+    @advisement = Advisement.find(params[:id])
+
+    respond_to do |format|
+      if @advisement.update(advisement_params)
+        format.html { redirect_to @advisement.banner_person, notice: 'advisement was successfully updated.' }
+        format.json { render :show, status: :ok, location: @advisement }
+      else
+        format.html { render :edit }
+        format.json { render json: @advisement.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def advisement_params
+      params.require(:advisement).permit(:date, :value)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   root to: 'banner_people#index'
   resources :banner_people, only: [:index, :show, :update]
+  resources :advisements, only: :update
 end
